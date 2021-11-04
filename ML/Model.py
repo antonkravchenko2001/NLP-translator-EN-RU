0d59185739ed25559eb881596422982e1cd1c1cf
Лена <antonio57banderas@gmail.com>
2021-11-05 00:38:26 +0300

    fixed logic and ui

---
+++ ML/Model.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import json
+import numpy as np
+
+
+with open('ML//dictionaries//input_dict.json', 'r') as fp:
+    input_dict = json.load(fp)
+with open('ML//dictionaries//target_dict_r.json', 'r') as fp:
+    target_dict_r = json.load(fp)
+encoder_model = tf.keras.models.load_model('ML//saved_models//encoder.h5')
+decoder_model = tf.keras.models.load_model('ML//saved_models//decoder.h5')
+
+
+def encode(seq):
+    seq = seq.lower()
+    seq = seq.split()
+    for i in range(len(seq)):
+        seq[i] = input_dict[seq[i]]
+    seq = np.array(seq)
+    seq = np.reshape(seq, (1, -1))
+    seq = tf.keras.preprocessing.sequence.pad_sequences(seq, maxlen=8, padding="pre")
+    return seq
+
+
+def translate(input_seq):
+    h1, c1 = encoder_model.predict(input_seq)
+    target_seq = np.zeros((1, 1))
+    target_seq[0, 0] = 1
+    eos = 2
+    output_sentence = []
+    current_state = [h1, c1, h1, c1]
+    for _ in range(20):
+        output_tokens, h3, c3, h4, c4 = decoder_model.predict([target_seq] + current_state)
+        idx = np.argmax(output_tokens[0, 0, :])
+        if eos == idx:
+            break
+        word = ''
+        if idx > 0:
+            word = target_dict_r[str(idx)]
+            output_sentence.append(word)
+        target_seq[0, 0] = idx
+        current_state = [h3, c4, h4, c4]
+    return ' '.join(output_sentence)
+
--- app.py
+++ app.py
@@ -1,26 +1,28 @@
-from flask import Flask, render_template, request
-from Model import encode, translate, input_dict
+from flask import Flask, render_template, request, abort
+from ML.Model import encode, translate, input_dict
 app = Flask(__name__)
+
+
+urls = ['translate']
 
 
 @app.route("/translator", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
         for word in request.form["sentence"].split():
-            if word not in input_dict.keys():
-                return render_template("translator.html", translation='No such word in the dictionary')
+            if word.lower() not in input_dict.keys():
+                return render_template("translator.html", translation=f'No such word in the dictionary: {word}')
         if request.form["sentence"] == "":
             return render_template("translator.html", translation="")
         translation = translate(encode(request.form["sentence"]))
         return render_template("translator.html", translation=translation)
     return render_template("translator.html")
 
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template('404.html', title = '404'), 404
+
+
 
 if __name__ == "__main__":
-    app.run()
-
-
-
-
-
-
+    app.run(debug=True)
